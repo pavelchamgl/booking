8df971128c4b1e9a8da597a5323f97472d5029a3
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2024-03-09 10:40:48 +0300

    Added cloudinary settings

--- config/config/settings.py
+++ config/config/settings.py
@@ -136,6 +136,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+CLOUDINARY_URL = os.getenv('CLOUDINARY_URL')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
