15c20a9d11bf3165fd69c55632058f851f60edc6
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2024-03-21 11:54:41 +0300

    Added account delete implementation

--- config/accounts/views.py
+++ config/accounts/views.py
@@ -2,6 +2,7 @@ from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from django.utils import timezone
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -351,3 +352,22 @@ class PasswordResetConfirmAPIView(APIView):
             return Response({'message': 'Password reset successfully.'}, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AccountDeletionAPIView(APIView):
+    """
+    API для удаления аккаунта пользователя.
+
+    Этот эндпоинт позволяет пользователям удалить свой аккаунт без подтверждения действия паролем.
+    """
+
+    permission_classes = (IsAuthenticated,)
+
+    @swagger_auto_schema(
+        responses={
+            204: openapi.Response(description="No Content - Аккаунт успешно удален."),
+        },
+    )
+    def delete(self, request):
+        request.user.delete()
+        return Response({'message': 'Аккаунт успешно удален.'}, status=status.HTTP_204_NO_CONTENT)
