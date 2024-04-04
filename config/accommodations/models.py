6ba0551c028c192a147f0256672effd55e829a3c
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2024-04-04 21:52:11 +0300

    Added Accommodation, AccommodationType, AccommodationImage and StayDate models

--- config/accommodations/models.py
+++ config/accommodations/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class AccommodationType(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Accommodation(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    city = models.CharField(max_length=100)
+    accommodation_type = models.ForeignKey(AccommodationType, on_delete=models.CASCADE)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    adults_capacity = models.IntegerField()
+    children_capacity = models.IntegerField()
+    breakfast_included = models.BooleanField(default=False)
+    kitchen_available = models.BooleanField(default=False)
+    bed_type = models.CharField(max_length=50)
+    wifi_available = models.BooleanField(default=True)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class AccommodationImage(models.Model):
+    accommodation = models.ForeignKey(Accommodation, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='accommodation_images/')
+
+
+class StayDate(models.Model):
+    accommodation = models.ForeignKey(Accommodation, related_name='stay_dates', on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
