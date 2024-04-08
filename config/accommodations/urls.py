787d331c2edd101387b0818283eff20f53b4724f
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2024-04-08 18:43:05 +0300

    Added url for implementation of detailed hotel view

--- config/accommodations/urls.py
+++ config/accommodations/urls.py
@@ -4,6 +4,7 @@ from .views import (
     AccommodationSearchAPIView,
     ToggleFavoriteAccommodationAPIView,
     FavoriteAccommodationListAPIView,
+    AccommodationDetailAPIView,
 )
 
 
@@ -12,4 +13,5 @@ urlpatterns = [
     path('accommodations/<int:pk>/toggle_favorite/', ToggleFavoriteAccommodationAPIView.as_view(),
          name='toggle-favorite-accommodation'),
     path('favorite_accommodations/', FavoriteAccommodationListAPIView.as_view(), name='favorite-accommodations-list'),
+    path('accommodations/<int:pk>/', AccommodationDetailAPIView.as_view(), name='accommodation-detail'),
 ]
