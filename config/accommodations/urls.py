19c96d28acd586181b32d7b4746d379ab32f688f
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2024-04-08 18:54:06 +0300

    Fixes urls

--- config/accommodations/urls.py
+++ config/accommodations/urls.py
@@ -9,9 +9,9 @@ from .views import (
 
 
 urlpatterns = [
-    path('accommodations/search/', AccommodationSearchAPIView.as_view(), name='accommodation-search'),
-    path('accommodations/<int:pk>/toggle_favorite/', ToggleFavoriteAccommodationAPIView.as_view(),
+    path('search/', AccommodationSearchAPIView.as_view(), name='accommodation-search'),
+    path('<int:pk>/toggle_favorite/', ToggleFavoriteAccommodationAPIView.as_view(),
          name='toggle-favorite-accommodation'),
     path('favorite_accommodations/', FavoriteAccommodationListAPIView.as_view(), name='favorite-accommodations-list'),
-    path('accommodations/<int:pk>/', AccommodationDetailAPIView.as_view(), name='accommodation-detail'),
+    path('<int:pk>/', AccommodationDetailAPIView.as_view(), name='accommodation-detail'),
 ]
