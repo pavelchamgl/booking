e1e8c7f634f9adb409ffca4aade2bc2f854c3780
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2024-05-16 20:27:34 +0300

    Update urls

--- config/config/settings.py
+++ config/config/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/neobooking/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 CLOUDINARY_URL = os.getenv('CLOUDINARY_URL')
